e394c6b2f6a17ce2d33d831b57f596b21ed32b07
CHINMAYA KUMAR DAS <121329700+Chinmayadas2003@users.noreply.github.com>
2023-11-14 21:49:15 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -34,7 +34,7 @@ model = tf.keras.models.load_model('weights.best.Resnet50.hdf5')
 # Function to preprocess the image
 def preprocess_image(image_path):
     img = Image.open(image_path)
-    img = img.resize((1, 1,2048))  # Adjust the size based on your model's input size
+    img = img.resize((1, 1))  # Adjust the size based on your model's input size
     img_array = tf.keras.preprocessing.image.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)  # Create batch axis
     #img_array /= 255.0  # Normalize pixel values
