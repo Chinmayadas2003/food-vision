f22d1bdb9bd2172623d28929050779eafa7955a3
CHINMAYA KUMAR DAS <121329700+Chinmayadas2003@users.noreply.github.com>
2023-11-14 22:05:03 +0530

    Update Hello.py 8.1

--- Hello.py
+++ Hello.py
@@ -27,58 +27,37 @@ def preprocess_image(image_path):
     img = img.resize((224, 224))  # Adjust the size based on your model's input size
     img_array = tf.keras.preprocessing.image.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)  # Create batch axis
-    # img_array /= 255.0  # Normalize pixel values
+    #img_array /= 255.0  # Normalize pixel values
     return img_array
 
 # Set page title and favicon
-st.set_page_config(
-    page_title="Food Vision App",
-    page_icon="🍔",
-    layout="wide",  # Wide layout for a more stylish appearance
-)
+st.set_page_config(page_title="Food Vision App", page_icon="🍔")
 
 # Streamlit App
 st.title('Food Vision Model Deployment')
+st.markdown("Upload an image to get predictions on the type of food!")
 
-# Sidebar with custom styling
-st.sidebar.title("Settings")
-st.sidebar.subheader("Choose an image to make predictions")
+uploaded_file = st.file_uploader("Choose an image...", type="jpg", key="fileUploader")
 
-# Main content area with custom styling
-main_column = st.beta_container()  # Use the newer st.beta_container for improved layout control
+if uploaded_file is not None:
+    # Display the uploaded image
+    st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
 
-with main_column:
-    # Upload file section with custom styling
-    uploaded_file = st.file_uploader("Upload an image...", type="jpg", key="fileUploader", help="Only JPG files are supported.")
+    # Preprocess the image
+    img_array = preprocess_image(uploaded_file)
 
-    if uploaded_file is not None:
-        # Display the uploaded image with rounded corners
-        st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True, output_format="auto", clamp=True, channels="RGB")
+    # Make predictions
+    predictions = model.predict(img_array)
+    class_index = np.argmax(predictions[0])
+    confidence = predictions[0][class_index]
 
-        # Preprocess the image
-        img_array = preprocess_image(uploaded_file)
+    # Display the results
+    st.subheader("Prediction:")
+    st.write(f"Class: {class_index}")
+    st.write(f"Confidence: {confidence:.2%}")
 
-        # Make predictions
-        predictions = model.predict(img_array)
-        class_index = np.argmax(predictions[0])
-        confidence = predictions[0][class_index]
-
-        # Display the results with custom styling
-        st.subheader("Prediction:")
-        st.write(f"Class: {class_index}")
-        st.write(f"Confidence: {confidence:.2%}")
-
-        # Add some additional styling for better visual appeal
-        st.success("Prediction successfully made!")
-        st.balloons()
-
-        # Customize the appearance of the success message
-        success_html = """
-        <div style="background-color:#d9f7be;padding:10px;border-radius:10px;">
-            <h3 style="color:#4caf50;">Success!</h3>
-            <p>Your prediction is ready.</p>
-        </div>
-        """
-        st.markdown(success_html, unsafe_allow_html=True)
+    # Add some styling for better visual appeal
+    st.success("Prediction successfully made!")
+    st.balloons()
 
 
